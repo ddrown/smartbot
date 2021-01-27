370bece711e9225fe1b04c3eeffac4934f01ae67
Dan Drown <dan-android@drown.org>
2021-01-26 19:49:10 -0600

    postmarket data

--- smartbot/finance.js
+++ smartbot/finance.js
@@ -4,8 +4,18 @@ async function getPrice(ticker) {
   const url = `https://query1.finance.yahoo.com/v10/finance/quoteSummary/${ticker}?modules=price`;
   const quote = await fetch(url).then(res => res.json());
   const price = quote.quoteSummary.result[0].price;
+
+  if(price.shortName === null || price.shortName === undefined) {
+    return `unable to get prices for ${ticker}`;
+  }
+
   const gainloss = price.regularMarketChange.raw > 0 ? `+${price.regularMarketChange.fmt}` : price.regularMarketChange.fmt;
-  return `${price.shortName} (${price.quoteType}:${price.exchange}/${price.symbol}) ${price.currencySymbol}${price.regularMarketPrice.fmt} ${price.currency} (${gainloss}, ${price.regularMarketChangePercent.fmt})`;
+  const regularStatus = `${price.shortName} (${price.quoteType}:${price.exchange}/${price.symbol}) ${price.currencySymbol}${price.regularMarketPrice.fmt} ${price.currency} (${gainloss}, ${price.regularMarketChangePercent.fmt})`;
+
+  const postGainloss = price.postMarketChange.raw > 0 ? `+${price.postMarketChange.fmt}` : price.postMarketChange.fmt;
+  const postMarketStatus = price.marketState === "POSTPOST" ? `aftermarket: ${price.currencySymbol}${price.postMarketPrice.fmt} (${postGainloss}, ${price.postMarketChangePercent.fmt})` : "";
+
+  return `${regularStatus} ${postMarketStatus} state=${price.marketState}`;
 }
 
 async function finance(client, respond, message) {
