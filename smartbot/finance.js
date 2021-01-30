e98b224e6d76e9a5d99a26d192a8472c128b5a07
Dan Drown <dan-android@drown.org>
2021-01-29 19:58:40 -0600

    before/after market data

--- smartbot/finance.js
+++ smartbot/finance.js
@@ -1,5 +1,27 @@
 const fetch = require('node-fetch');
 
+function postMarket(price) {
+  if(price.marketState === "REGULAR") {
+    return "";
+  }
+  if(price.marketState === "PRE") {
+    const preGainloss = price.preMarketChange.raw > 0 ? `+${price.preMarketChange.fmt}` : price.preMarketChange.fmt;
+    return `beforemarket: ${price.currencySymbol}${price.preMarketPrice.fmt} (${preGainloss}, ${price.preMarketChangePercent.fmt})`;
+  }
+  if(price.marketState !== "POST" && price.marketState !== "POSTPOST" && price.marketState !== "PREPRE") {
+    return "";
+  }
+  const postGainloss = price.postMarketChange.raw > 0 ? `+${price.postMarketChange.fmt}` : price.postMarketChange.fmt;
+  return `aftermarket: ${price.currencySymbol}${price.postMarketPrice.fmt} (${postGainloss}, ${price.postMarketChangePercent.fmt})`;
+}
+
+function strState(price) {
+  if(price.marketState === "PRE" || price.marketState === "POST" || price.marketState === "PREPRE" || price.marketState === "POSTPOST" || price.marketState === "REGULAR") {
+    return "";
+  }
+  return `state=${price.marketState}`;
+}
+
 async function getPrice(ticker) {
   const url = `https://query1.finance.yahoo.com/v10/finance/quoteSummary/${ticker}?modules=price`;
   const quote = await fetch(url).then(res => res.json());
@@ -12,10 +34,10 @@ async function getPrice(ticker) {
   const gainloss = price.regularMarketChange.raw > 0 ? `+${price.regularMarketChange.fmt}` : price.regularMarketChange.fmt;
   const regularStatus = `${price.shortName} (${price.quoteType}:${price.exchange}/${price.symbol}) ${price.currencySymbol}${price.regularMarketPrice.fmt} ${price.currency} (${gainloss}, ${price.regularMarketChangePercent.fmt})`;
 
-  const postGainloss = price.postMarketChange.raw > 0 ? `+${price.postMarketChange.fmt}` : price.postMarketChange.fmt;
-  const postMarketStatus = price.marketState === "POSTPOST" ? `aftermarket: ${price.currencySymbol}${price.postMarketPrice.fmt} (${postGainloss}, ${price.postMarketChangePercent.fmt})` : "";
+  const postMarketStatus = postMarket(price);
+  const state = strState(price);
 
-  return `${regularStatus} ${postMarketStatus} state=${price.marketState}`;
+  return `${regularStatus} ${postMarketStatus} ${state}`;
 }
 
 async function finance(client, respond, message) {
@@ -24,7 +46,7 @@ async function finance(client, respond, message) {
     client.say(respond, "!finance [symbol]");
     return;
   }
-  const ticker = command[1];
+  const ticker = command[1].toUpperCase();
   if (!ticker.match(/^[A-Z0-9.]+$/)) {
     client.say(respond, "invalid characters in ticker name");
     return;
